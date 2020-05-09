de0ea0a8c4dac527570d88fd58a622392a65356a
Paredes <nico.paredes05@gmail.com>
2020-05-08 23:25:01 -0400

    ADD: get directions using a initial coord

--- coords.py
+++ coords.py
@@ -14,6 +14,27 @@ def get_all_init_coords(dimension):
         list_coords.append(opposite_coord(c))
     return list_coords
 
+# up_right = -1,1 up_left = -1,-1 down_right = 1,1 down_left = 1,-1
+def get_directions(
+        dim, init_coord):
+    directions = [[-1,1], [-1,-1], [1,1], [1,-1]]
+    if init_coord in get_all_init_coords(dim):
+        if init_coord[0] < 0:
+            return [c for c in directions if c[0] == 1]
+        elif init_coord[1] < 0:
+            return [c for c in directions if c[1] == 1]
+        elif init_coord[0] < init_coord[1]:
+            return [c for c in directions if c[1] == -1]
+        elif init_coord[0] > init_coord[1]:
+            return [c for c in directions if c[0] == -1]
+        else:
+            return []
+    else:
+        return []
+
+t = get_all_init_coords(5)
+print(t[0])
+print(get_directions(5,t[0]))
 
 
 
